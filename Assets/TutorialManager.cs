f0e5c5fefe42ed41b04dc608c8f853b3a169868f
nepal143 <71310255+nepal143@users.noreply.github.com>
2025-04-17 17:33:00 +0530

    added tutorial screens

--- Assets/TutorialManager.cs
+++ Assets/TutorialManager.cs
@@ -1,35 +1,30 @@
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
 
 public class TutorialManager : MonoBehaviour
 {
-    public GameObject[] tutorialScreens; // Assign each tutorial panel in order
-    public Button[] nextButtons;         // Assign the "Next" buttons from each screen
-    public GameObject tapToStartScreen;  // Final screen with "Tap to Start"
+    public GameObject[] tutorialScreens; // Include all screens, last one is "Tap to Start"
 
     private int currentScreenIndex = 0;
-    private bool waitingForTap = false;
 
     void Start()
     {
         Time.timeScale = 0; // Pause the game
-        ShowScreen(0);
-
-        // Add listeners to next buttons
-        for (int i = 0; i < nextButtons.Length; i++)
-        {
-            int index = i;
-            nextButtons[i].onClick.AddListener(() => ShowNextScreen(index));
-        }
+        ShowScreen(0);      // Show first screen
     }
 
     void Update()
     {
-        if (waitingForTap && Input.GetMouseButtonDown(0) && !IsPointerOverUI())
+        if (Input.GetMouseButtonDown(0))
         {
-            StartGame();
+            // If on last screen, start game
+            if (currentScreenIndex >= tutorialScreens.Length - 1)
+            {
+                StartGame();
+            }
+            else
+            {
+                ShowNextScreen();
+            }
         }
     }
 
@@ -39,42 +34,17 @@ public class TutorialManager : MonoBehaviour
         {
             tutorialScreens[i].SetActive(i == index);
         }
-
-        tapToStartScreen.SetActive(false);
     }
 
-    void ShowNextScreen(int index)
+    void ShowNextScreen()
     {
-        if (index + 1 < tutorialScreens.Length)
-        {
-            ShowScreen(index + 1);
-        }
-        else
-        {
-            ShowFinalScreen();
-        }
-    }
-
-    void ShowFinalScreen()
-    {
-        foreach (GameObject screen in tutorialScreens)
-        {
-            screen.SetActive(false);
-        }
-
-        tapToStartScreen.SetActive(true);
-        waitingForTap = true;
+        currentScreenIndex++;
+        ShowScreen(currentScreenIndex);
     }
 
     void StartGame()
     {
-        tapToStartScreen.SetActive(false);
+        tutorialScreens[currentScreenIndex].SetActive(false);
         Time.timeScale = 1; // Resume the game
-        waitingForTap = false;
-    }
-
-    bool IsPointerOverUI()
-    {
-        return EventSystem.current.IsPointerOverGameObject();
     }
 }
